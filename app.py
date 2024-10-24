1f2744d0a66220df245da16892c8744ac62c705f
cesaruribe <cesar.uribet@gmail.com>
2024-10-23 21:00:42 -0500

    Version Produccion

--- app.py
+++ app.py
@@ -40,5 +40,5 @@ def errorPDF():
     return render_template('errorPDF.html')
 
 if __name__ == '__main__':
-    #app.run(debug=True)
-    app.run(host="0.0.0.0",port=80)
+    app.run(debug=True)
+    #app.run(host="0.0.0.0",port=80)
