4c917de00bf00b71feca7b90ab31387ca2fbf981
cesaruribe <cesar.uribet@gmail.com>
2024-10-24 23:39:53 -0500

    Programa con Historia

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ def index():
 def ingresarDatos():
     return render_template('formularioEcografia.html')
 
+@app.route('/formularioH')
+def ingresarDatosH():
+    return render_template('formularioHistoria.html')
+
 @app.route('/home')
 def inicio():
     return render_template('home.html')
@@ -21,10 +25,26 @@ def generar_pdf():
         subdirectorio = "informesPDF/"
         data = request.form
         # Construye el nombre del archivo
-        nombre_archivo = f"{subdirectorio}informe_{data['cedula']}_{data['fecha']}.pdf"
+        nombre_archivo = f"{subdirectorio}informe_{data['cedula']}_{data['apellido']}_{data['fecha']}.pdf"
         pdfkit.from_string(render_template('informe.html', **data), nombre_archivo, 
                         options={"page-size": "Letter", "encoding": "UTF-8"})
         return redirect(url_for('informe'))
+        #return jsonify({"message": "PDF generado exitosamente"})
+    except Exception as e:
+        app.logger.error(f"Error al generar PDF: {e}")
+        return redirect(url_for('errorPDF'))
+        #return jsonify({"error": "Error al generar PDF"}), 500
+
+@app.route('/generarH_pdf', methods=['POST'])
+def generarH_pdf():
+    try:
+        subdirectorio = "informesPDF/"
+        data = request.form
+        # Construye el nombre del archivo
+        nombre_archivo = f"{subdirectorio}Hinforme_{data['cedula']}_{data['apellido']}_{data['fecha']}.pdf"
+        pdfkit.from_string(render_template('informeH.html', **data), nombre_archivo, 
+                        options={"page-size": "Letter", "encoding": "UTF-8"})
+        return redirect(url_for('informeH'))
         #return jsonify({"message": "PDF generado exitosamente"})
     except Exception as e:
         app.logger.error(f"Error al generar PDF: {e}")
@@ -35,9 +55,17 @@ def generar_pdf():
 def informe():
     return render_template('informe.html')
 
+@app.route('/informeH')
+def informeH():
+    return render_template('informeH.html')
+
 @app.route('/errorPDF')
 def errorPDF():
     return render_template('errorPDF.html')
+
+@app.route('/contacto')
+def contacto():
+    return render_template('contacto.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
